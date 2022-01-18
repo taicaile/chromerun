56021fa6563e99c07ef3f1f7631c48b3470651f9
nobody <23215696+taicaile@users.noreply.github.com>
2022-01-18 09:55:31 +0800

    update

--- chromerun.sh
+++ chromerun.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
-for PROFILE in ~/.config/google-chrome/Profile*
-do
-    google-chrome --profile-directory="$(basename "$PROFILE")" >/dev/null 2>&1
+# make sure that no chrome is running when this script is executed.
+# start chrome -> wait 5 seconds -> kill
+
+for PROFILE in ~/.config/google-chrome/Profile*; do
+    basename "$PROFILE"
+    google-chrome --profile-directory="$(basename "$PROFILE")" >/dev/null 2>&1 &
+    sleep 5
+    kill -SIGTERM $!
 done
 
-sleep 5
-
 # echo "${PIDS[@]}" | xargs -r kill -SIGTERM "$1"
-pgrep chrome | xargs -r kill -SIGTERM
+# pgrep chrome | xargs -r kill -SIGTERM
+
+# notes
+
+# No chrome is running, the `google-chrome` command is ran in foreground.
+# Otherwise, the `google-chrome` command is ran in background.
